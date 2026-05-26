64a3dfa74694d2b7b56fc0c2c98a726f8bcfa0e5
FlatBass <low.flatbass@gmail.com>
2026-05-26 21:12:04 +0900

    feat : javascript 260526

---
+++ JavaScript/script-2.js
@@ -0,0 +1,27 @@
+/* 기본형 */
+// let 변수명
+let width;
+let height;
+
+width = 200;
+height = 50;
+let area = width * height;
+console.log(area);
+
+/* 상수 */
+const PI = 3.14;
+let radius = prompt("원의 반지름을 입력하세요.");
+let circleArea = PI * radius * radius;
+console.log(circleArea);
+
+/* 템플릿 리터럴 */
+let name = 'Kim'
+let classRoom = 204
+console.log(`${name}님, ${classRoom}호 강의실로 입장하세요.`)
+
+/* 배열 */
+let spring = '봄';
+let summer = '여름';
+let autumn = '가을';
+let winter = '겨울';
+let seasons = ['봄', '여름', '가을', '겨울'];
